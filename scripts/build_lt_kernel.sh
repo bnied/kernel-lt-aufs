6d50474654f457bbf268be9b8ca258f1d18d9d7e
Ben Nied <spacewreckage@gmail.com>
2016-12-09 07:44:41 -0500

    fix script header

--- scripts/build_lt_kernel.sh
+++ scripts/build_lt_kernel.sh
@@ -80,7 +80,7 @@ if [ ! -f ../configs-el$EL_VERSION/config-$FULL_VERSION-$ARCH ]; then
 fi
 
 # Announce what we've been asked to build LOUDLY
-printf "*********** KERNEL-ML-AUFS BUILD COMMENCING ***********"
+printf "*********** KERNEL-LT-AUFS BUILD COMMENCING ***********"
 printf "\n\n\t* Version:\t$FULL_VERSION"
 printf "\n\t* Architecture:\t$ARCH"
 printf "\n\t* EL Version:\t$EL_VERSION"
